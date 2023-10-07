4136c869e2a4efb69abb929db736766cbb96f181
IbrahimXXs <146712453+IbrahimXXs@users.noreply.github.com>
2023-10-07 18:07:43 +0400

    Update Full Game.py

--- Full Game.py
+++ Full Game.py
@@ -1,52 +1,28 @@
-import random 
- 
-# returns either 'rock', 'paper' or 'scissors'
+import random
+
 def get_computers_choice():
-    random_number = random.randint(1, 3)
- 
-    options = {1: 'rock', 2: 'paper', 3: 'scissors'}
-    computers_choice = options[random_number]
- 
+    options = ['rock', 'paper', 'scissors']
+    computers_choice = random.choice(options)
     return computers_choice
- 
-# take user input and return it
+
 def get_user_input():
-    user_input = input('Enter rock/paper/scissors: ')
- 
-    # converting the user input to lowercase
-    user_input = user_input.lower() 
- 
-    return user_input
- 
-# return either 'win', 'lose' or 'draw'
+    while True:
+        user_input = input('Enter rock/paper/scissors: ').lower()
+        if user_input in ['rock', 'paper', 'scissors']:
+            return user_input
+
 def get_result(user_pick, computer_pick):
-    
     if computer_pick == user_pick:
         return 'draw'
-    
-    elif user_pick == 'paper' and computer_pick == 'rock':
+    elif (user_pick == 'paper' and computer_pick == 'rock') or (user_pick == 'rock' and computer_pick == 'scissors') or (user_pick == 'scissors' and computer_pick == 'paper'):
         return 'win'
-    elif user_pick == 'rock' and computer_pick == 'scissors':
-        return 'win'
-    elif user_pick == 'scissors' and computer_pick == 'paper':
-        return 'win'
-    
     else:
         return 'lose'
- 
- 
-# outside of the function
+
 computer_pick = get_computers_choice()
- 
-# get input until user enters 'rock', 'paper' or scissors
-while True:
-    users_pick = get_user_input()
-    if users_pick in ['rock', 'paper', 'scissors']:
-        break
- 
+users_pick = get_user_input()
 result = get_result(users_pick, computer_pick)
- 
-# printing result
+
 print(f"Computer's pick: {computer_pick}")
 print(f"Your pick: {users_pick}")
 print(f"You {result}")
